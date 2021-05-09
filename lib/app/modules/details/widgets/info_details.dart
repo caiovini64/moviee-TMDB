6232ad62669f1cf78b0e94e6ee6f0416910825d0
Caio Jesus <caioj3505@gmail.com>
2021-05-09 19:44:34 -0300

    Adciona detalhes principais tela de detalhes

--- lib/app/modules/details/widgets/info_details.dart
+++ lib/app/modules/details/widgets/info_details.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tmdb_api/app/modules/details/controller.dart';
+import 'package:tmdb_api/app/theme/app_theme.dart';
 
 class InfoDetails extends GetView<DetailsController> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: ClipRRect(
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipRRect(
             borderRadius: BorderRadius.circular(10),
             child: Image(
               image: NetworkImage(
                   controller.urlPath + controller.movie.backdropPath),
             ),
           ),
-        ),
-      ],
+          SizedBox(height: 30),
+          Text(
+            controller.movie.title,
+            style: AppTheme.to.theme.textTheme.headline1,
+          ),
+          SizedBox(height: 18),
+          Text(
+            controller.movie.overview,
+            style: AppTheme.to.theme.textTheme.bodyText1,
+          ),
+        ],
+      ),
     );
   }
 }
