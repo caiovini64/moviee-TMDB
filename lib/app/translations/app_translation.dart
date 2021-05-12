e0ce0042b140de54fa85d167c90e0b291eef1bf0
Caio_Jesus_Windows <caio.nascimento@grupofasitec.com.br>
2021-05-12 14:01:22 -0300

    Retira internacionalização

--- lib/app/translations/app_translation.dart
+++
@@ -1,7 +0,0 @@
-import 'package:tmdb_api/app/translations/pt_BR/pt_br_translations.dart';
-
-abstract class AppTranslation {
-  static Map<String, Map<String, String>> translations = {
-    'pt_BR': ptBr,
-  };
-}
--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,13 @@ import 'package:get/get.dart';
 import 'package:tmdb_api/app/modules/splash/binding.dart';
 import 'package:tmdb_api/app/routes/app_pages.dart';
 import 'package:tmdb_api/app/routes/app_routes.dart';
-import 'package:tmdb_api/app/translations/app_translation.dart';
 
 void main() {
   runApp(GetMaterialApp(
-    locale: Locale('pt', 'BR'),
     debugShowCheckedModeBanner: false,
     initialRoute: Routes.HOME,
     defaultTransition: Transition.fade,
     initialBinding: SplashBinding(),
     getPages: AppPages.pages,
-    translationsKeys: AppTranslation.translations,
   ));
 }
