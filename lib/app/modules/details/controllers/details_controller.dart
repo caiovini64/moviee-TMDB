73113c94a823c47ed9e5d9159267a6941b0a83b8
Caio Jesus <caioj3505@gmail.com>
2021-05-16 17:32:39 -0300

    Atualiza filme clickado na pagina de detalhes

--- lib/app/modules/details/controllers/details_controller.dart
+++ lib/app/modules/details/controllers/details_controller.dart
@@ -1,11 +1,25 @@
 import 'package:get/get.dart';
 import 'package:tmdb_api/app/data/models/movie_model.dart';
+import 'package:tmdb_api/app/data/repositories/movie_repository.dart';
 
-class DetailsController extends GetxController {
+class DetailsController extends GetxController with StateMixin {
   MovieModel _movie = Get.arguments;
   final urlPath = 'https://image.tmdb.org/t/p/w500/';
 
   MovieModel get movie => _movie;
 
   updateCurrentMovie(movie) => _movie = movie.value;
+
+  void onInit() {
+    super.onInit();
+    loadDetails(_movie.id);
+  }
+
+  loadDetails(movieId) async {
+    await MovieRepository.getDetails(movieId).then((value) {
+      change(value, status: RxStatus.success());
+    }).catchError((error) {
+      change(null, status: RxStatus.error('Erro ao carregar detalhes'));
+    });
+  }
 }
